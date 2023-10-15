b06f84494aa8bf64136681dab4b95f4bab75e408
paco <68790836+PacichaBG@users.noreply.github.com>
2023-10-15 22:48:03 +0300

    a

--- FitVitality/FitVitality/Main.Designer.cs
+++ FitVitality/FitVitality/Main.Designer.cs
@@ -29,24 +29,13 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            panel1 = new Panel();
             SuspendLayout();
-            // 
-            // panel1
-            // 
-            panel1.BackgroundImage = (Image)resources.GetObject("panel1.BackgroundImage");
-            panel1.BackgroundImageLayout = ImageLayout.Stretch;
-            panel1.Location = new Point(12, 12);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(53, 42);
-            panel1.TabIndex = 0;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(750, 463);
-            Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
@@ -60,7 +49,5 @@
         }
 
         #endregion
-
-        private Panel panel1;
     }
 }

f111d69a56e84005b734cba5d7059ee40705c9b0
paco <68790836+PacichaBG@users.noreply.github.com>
2023-10-15 22:56:14 +0300

    a

---
+++ FitVitality/FitVitality/WelcomeScreen.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace FitVitality
+{
+    partial class WelcomeScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WelcomeScreen));
+            SuspendLayout();
+            // 
+            // WelcomeScreen
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(750, 463);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "WelcomeScreen";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "WelcomeScreen";
+            Load += WelcomeScreen_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}

06cd70a86f59b8463b6f1ca125ab77f3130a98a6
Martin Hristov <52611243+mhrstv@users.noreply.github.com>
2023-11-13 17:54:25 +0200

    a

--- FitVitality/FitVitality/home.Designer.cs
+++ FitVitality/FitVitality/home.Designer.cs
@@ -28,20 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            bmicalc_label = new Label();
+            bmi_label = new Label();
+            bmicategory_label = new Label();
+            bmi_panel = new Panel();
+            bmi_panel.SuspendLayout();
             SuspendLayout();
+            // 
+            // bmicalc_label
+            // 
+            bmicalc_label.AutoSize = true;
+            bmicalc_label.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            bmicalc_label.ForeColor = Color.FromArgb(92, 225, 230);
+            bmicalc_label.Location = new Point(57, 10);
+            bmicalc_label.Name = "bmicalc_label";
+            bmicalc_label.Size = new Size(146, 22);
+            bmicalc_label.TabIndex = 0;
+            bmicalc_label.Text = "BMI Calculator";
+            // 
+            // bmi_label
+            // 
+            bmi_label.Font = new Font("Calibri", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            bmi_label.Location = new Point(52, 133);
+            bmi_label.Name = "bmi_label";
+            bmi_label.Size = new Size(156, 23);
+            bmi_label.TabIndex = 1;
+            bmi_label.Text = "{BMI}";
+            bmi_label.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // bmicategory_label
+            // 
+            bmicategory_label.Font = new Font("Calibri", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            bmicategory_label.Location = new Point(28, 32);
+            bmicategory_label.Name = "bmicategory_label";
+            bmicategory_label.Size = new Size(205, 23);
+            bmicategory_label.TabIndex = 2;
+            bmicategory_label.Text = "{Category}";
+            bmicategory_label.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // bmi_panel
+            // 
+            bmi_panel.Controls.Add(bmi_label);
+            bmi_panel.Controls.Add(bmicategory_label);
+            bmi_panel.Controls.Add(bmicalc_label);
+            bmi_panel.Location = new Point(418, 12);
+            bmi_panel.Name = "bmi_panel";
+            bmi_panel.Size = new Size(260, 164);
+            bmi_panel.TabIndex = 3;
             // 
             // home
             // 
             AutoScaleDimensions = new SizeF(96F, 96F);
             AutoScaleMode = AutoScaleMode.Dpi;
-            BackColor = Color.Red;
+            BackColor = Color.FromArgb(250, 252, 252);
             ClientSize = new Size(690, 368);
+            Controls.Add(bmi_panel);
             FormBorderStyle = FormBorderStyle.None;
             Name = "home";
             Text = "home";
+            Activated += home_Activated;
+            Load += home_Load;
+            bmi_panel.ResumeLayout(false);
+            bmi_panel.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Label bmicalc_label;
+        private Label bmi_label;
+        private Label bmicategory_label;
+        private Panel bmi_panel;
     }
 }
--- FitVitality/FitVitality/home.cs
+++ FitVitality/FitVitality/home.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,207 @@ namespace FitVitality
 {
     public partial class home : Form
     {
+        List<Panel> active_tabs = new List<Panel>();
+        private string name;
+        private int age;
+        private string gender;
+        private string weight;
+        private string height;
+        private string goal;
+        private string unit_selection;
+        public string userID;
+        public double bmi;
+        string connectionString = @"Server=tcp:fitvitality.database.windows.net,1433;Initial Catalog=FitVitality-AWS;Persist Security Info=False;User ID=fitvitality;Password=adminskaparola123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
         public home()
         {
             InitializeComponent();
+        }
+
+        private void home_Load(object sender, EventArgs e)
+        {
+            var cfg = new Config("FitVitality.ini");
+            userID = cfg.Read("UserID", "SETTINGS");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT * FROM UserSettings WHERE UserID = @UserID";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@UserID", userID);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            name = reader["Name"].ToString();
+                            age = int.Parse(reader["Age"].ToString());
+                            if (reader["Gender"].ToString() == "Male")
+                            {
+                                gender = "Male";
+                            }
+                            else
+                            {
+                                gender = "Female";
+                            }
+                            weight = reader["Weight"].ToString();
+                            height = reader["Height"].ToString();
+                            if (reader["Goal"].ToString() == "Cut")
+                            {
+                                goal = "Cut";
+                            }
+                            else if (reader["Goal"].ToString() == "Maintain")
+                            {
+                                goal = "Maintain";
+                            }
+                            else
+                            {
+                                goal = "Bulk";
+                            }
+                        }
+                    }
+                }
+            }
+            bmi = Math.Round((Convert.ToDouble(weight) / Math.Pow(Convert.ToDouble(height) / 100, 2)), 2);
+            bmi_label.Text = $"BMI = {bmi.ToString()} kg/m²";
+            if (age < 20 && age > 2)
+            {
+
+            }
+            else
+            {
+                if (bmi < 16)
+                {
+                    bmicategory_label.Text = "(Severe Thinness)";
+                    bmicategory_label.ForeColor = Color.FromArgb(188, 32, 32);
+                }
+                else if (bmi >= 16 && bmi < 17)
+                {
+                    bmicategory_label.Text = "(Moderate Thinness)";
+                    bmicategory_label.ForeColor = Color.FromArgb(211, 136, 136);
+                }
+                else if (bmi >= 17 && bmi < 18.5)
+                {
+                    bmicategory_label.Text = "(Mild Thinness)";
+                    bmicategory_label.ForeColor = Color.FromArgb(255, 228, 0);
+                }
+                else if (bmi >= 18.5 && bmi < 25)
+                {
+                    bmicategory_label.Text = "(Normal)";
+                    bmicategory_label.ForeColor = Color.FromArgb(0, 129, 55);
+                }
+                else if (bmi >= 25 && bmi < 30)
+                {
+                    bmicategory_label.Text = "(Overweight)";
+                    bmicategory_label.ForeColor = Color.FromArgb(255, 228, 0);
+                }
+                else if (bmi >= 30 && bmi < 35)
+                {
+                    bmicategory_label.Text = "(Obese Class I)";
+                    bmicategory_label.ForeColor = Color.FromArgb(211, 136, 136);
+                }
+                else if (bmi >= 35 && bmi < 40)
+                {
+                    bmicategory_label.Text = "(Obese Class II)";
+                    bmicategory_label.ForeColor = Color.FromArgb(188, 32, 32);
+                }
+                else
+                {
+                    bmicategory_label.Text = "(Obese Class III)";
+                    bmicategory_label.ForeColor = Color.FromArgb(138, 1, 1);
+                }
+            }
+        }
+
+        private void home_Activated(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT * FROM UserSettings WHERE UserID = @UserID";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@UserID", userID);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            name = reader["Name"].ToString();
+                            age = int.Parse(reader["Age"].ToString());
+                            if (reader["Gender"].ToString() == "Male")
+                            {
+                                gender = "Male";
+                            }
+                            else
+                            {
+                                gender = "Female";
+                            }
+                            weight = reader["Weight"].ToString();
+                            height = reader["Height"].ToString();
+                            if (reader["Goal"].ToString() == "Cut")
+                            {
+                                goal = "Cut";
+                            }
+                            else if (reader["Goal"].ToString() == "Maintain")
+                            {
+                                goal = "Maintain";
+                            }
+                            else
+                            {
+                                goal = "Bulk";
+                            }
+                        }
+                    }
+                }
+            }
+            bmi = Math.Round((Convert.ToDouble(weight) / Math.Pow(Convert.ToDouble(height) / 100, 2)), 2);
+            bmi_label.Text = $"BMI = {bmi.ToString()} kg/m²";
+            if (age < 20 && age > 2)
+            {
+
+            }
+            else
+            {
+                if (bmi < 16)
+                {
+                    bmicategory_label.Text = "(Severe Thinness)";
+                    bmicategory_label.ForeColor = Color.FromArgb(188, 32, 32);
+                }
+                else if (bmi >= 16 && bmi < 17)
+                {
+                    bmicategory_label.Text = "(Moderate Thinness)";
+                    bmicategory_label.ForeColor = Color.FromArgb(211, 136, 136);
+                }
+                else if (bmi >= 17 && bmi < 18.5)
+                {
+                    bmicategory_label.Text = "(Mild Thinness)";
+                    bmicategory_label.ForeColor = Color.FromArgb(255, 228, 0);
+                }
+                else if (bmi >= 18.5 && bmi < 25)
+                {
+                    bmicategory_label.Text = "(Normal)";
+                    bmicategory_label.ForeColor = Color.FromArgb(0, 129, 55);
+                }
+                else if (bmi >= 25 && bmi < 30)
+                {
+                    bmicategory_label.Text = "(Overweight)";
+                    bmicategory_label.ForeColor = Color.FromArgb(255, 228, 0);
+                }
+                else if (bmi >= 30 && bmi < 35)
+                {
+                    bmicategory_label.Text = "(Obese Class I)";
+                    bmicategory_label.ForeColor = Color.FromArgb(211, 136, 136);
+                }
+                else if (bmi >= 35 && bmi < 40)
+                {
+                    bmicategory_label.Text = "(Obese Class II)";
+                    bmicategory_label.ForeColor = Color.FromArgb(188, 32, 32);
+                }
+                else
+                {
+                    bmicategory_label.Text = "(Obese Class III)";
+                    bmicategory_label.ForeColor = Color.FromArgb(138, 1, 1);
+                }
+            }
         }
     }
 }

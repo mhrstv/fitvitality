b48f7623c99ba16824591d9715ad96826e43c27f
Martin Hristov <52611243+mhrstv@users.noreply.github.com>
2023-12-20 13:47:31 +0200

    aaa

--- FitVitality/FitVitality/Workouts.Designer.cs
+++ FitVitality/FitVitality/Workouts.Designer.cs
@@ -50,19 +50,6 @@
             nextButton2 = new Krypton.Toolkit.KryptonButton();
             muscleGroupsLabel = new Label();
             muscleGroupsBorders = new PictureBox();
-            panel1 = new Panel();
-            panel2 = new Panel();
-            kryptonButton1 = new Krypton.Toolkit.KryptonButton();
-            label1 = new Label();
-            pictureBox1 = new PictureBox();
-            panel3 = new Panel();
-            panel4 = new Panel();
-            kryptonButton2 = new Krypton.Toolkit.KryptonButton();
-            pictureBox2 = new PictureBox();
-            pictureBox3 = new PictureBox();
-            pictureBox4 = new PictureBox();
-            label2 = new Label();
-            pictureBox5 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)leftCorner).BeginInit();
             ((System.ComponentModel.ISupportInitialize)rightCorner).BeginInit();
             ((System.ComponentModel.ISupportInitialize)vLine1).BeginInit();
@@ -78,15 +65,6 @@
             muscleGroupsPanel.SuspendLayout();
             nextButtonPanel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)muscleGroupsBorders).BeginInit();
-            panel1.SuspendLayout();
-            panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            panel3.SuspendLayout();
-            panel4.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
             SuspendLayout();
             // 
             // leftCorner
@@ -420,250 +398,12 @@
             muscleGroupsBorders.TabStop = false;
             muscleGroupsBorders.Click += muscleGroupsBorders_Click;
             // 
-            // panel1
-            // 
-            panel1.Controls.Add(panel2);
-            panel1.Controls.Add(label1);
-            panel1.Controls.Add(pictureBox1);
-            panel1.Location = new Point(86, 70);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(518, 262);
-            panel1.TabIndex = 35;
-            panel1.Visible = false;
-            // 
-            // panel2
-            // 
-            panel2.BackColor = Color.WhiteSmoke;
-            panel2.Controls.Add(kryptonButton1);
-            panel2.Location = new Point(214, 221);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(90, 25);
-            panel2.TabIndex = 32;
-            // 
-            // kryptonButton1
-            // 
-            kryptonButton1.CornerRoundingRadius = 15F;
-            kryptonButton1.Location = new Point(0, 0);
-            kryptonButton1.Name = "kryptonButton1";
-            kryptonButton1.PaletteMode = Krypton.Toolkit.PaletteMode.ProfessionalSystem;
-            kryptonButton1.Size = new Size(90, 25);
-            kryptonButton1.StateCommon.Back.Color1 = Color.FromArgb(92, 225, 230);
-            kryptonButton1.StateCommon.Back.Color2 = Color.FromArgb(92, 225, 230);
-            kryptonButton1.StateCommon.Back.ColorAngle = 45F;
-            kryptonButton1.StateCommon.Border.Color1 = Color.FromArgb(92, 225, 230);
-            kryptonButton1.StateCommon.Border.Color2 = Color.FromArgb(92, 225, 230);
-            kryptonButton1.StateCommon.Border.ColorAngle = 45F;
-            kryptonButton1.StateCommon.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
-            kryptonButton1.StateCommon.Border.GraphicsHint = Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
-            kryptonButton1.StateCommon.Border.Rounding = 15F;
-            kryptonButton1.StateCommon.Border.Width = 1;
-            kryptonButton1.StateCommon.Content.ShortText.Color1 = Color.White;
-            kryptonButton1.StateCommon.Content.ShortText.Color2 = Color.White;
-            kryptonButton1.StateCommon.Content.ShortText.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Bold, GraphicsUnit.Point);
-            kryptonButton1.StateDisabled.Back.Color1 = Color.FromArgb(92, 225, 230);
-            kryptonButton1.StateDisabled.Back.Color2 = Color.FromArgb(92, 225, 230);
-            kryptonButton1.StateDisabled.Border.Color1 = Color.FromArgb(92, 225, 230);
-            kryptonButton1.StateDisabled.Border.Color2 = Color.FromArgb(92, 225, 230);
-            kryptonButton1.StateDisabled.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
-            kryptonButton1.StateNormal.Back.Color1 = Color.FromArgb(92, 225, 230);
-            kryptonButton1.StateNormal.Back.Color2 = Color.FromArgb(92, 225, 230);
-            kryptonButton1.StateNormal.Back.ColorAngle = 45F;
-            kryptonButton1.StateNormal.Back.GraphicsHint = Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
-            kryptonButton1.StateNormal.Border.Color1 = Color.FromArgb(92, 225, 230);
-            kryptonButton1.StateNormal.Border.Color2 = Color.FromArgb(92, 225, 230);
-            kryptonButton1.StateNormal.Border.ColorAngle = 45F;
-            kryptonButton1.StateNormal.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
-            kryptonButton1.StateNormal.Border.GraphicsHint = Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
-            kryptonButton1.StateNormal.Border.Rounding = 15F;
-            kryptonButton1.StateNormal.Border.Width = 1;
-            kryptonButton1.StatePressed.Back.Color1 = Color.FromArgb(0, 160, 192);
-            kryptonButton1.StatePressed.Back.Color2 = Color.FromArgb(0, 160, 192);
-            kryptonButton1.StatePressed.Back.ColorAngle = 45F;
-            kryptonButton1.StatePressed.Back.GraphicsHint = Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
-            kryptonButton1.StatePressed.Border.Color1 = Color.FromArgb(0, 160, 192);
-            kryptonButton1.StatePressed.Border.Color2 = Color.FromArgb(0, 160, 192);
-            kryptonButton1.StatePressed.Border.ColorAngle = 45F;
-            kryptonButton1.StatePressed.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
-            kryptonButton1.StatePressed.Border.GraphicsHint = Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
-            kryptonButton1.StatePressed.Border.Rounding = 15F;
-            kryptonButton1.StatePressed.Border.Width = 1;
-            kryptonButton1.StateTracking.Back.Color1 = Color.FromArgb(161, 234, 230);
-            kryptonButton1.StateTracking.Back.Color2 = Color.FromArgb(161, 234, 230);
-            kryptonButton1.StateTracking.Back.ColorAngle = 45F;
-            kryptonButton1.StateTracking.Back.GraphicsHint = Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
-            kryptonButton1.StateTracking.Border.Color1 = Color.FromArgb(161, 234, 230);
-            kryptonButton1.StateTracking.Border.Color2 = Color.FromArgb(161, 234, 230);
-            kryptonButton1.StateTracking.Border.ColorAngle = 45F;
-            kryptonButton1.StateTracking.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
-            kryptonButton1.StateTracking.Border.GraphicsHint = Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
-            kryptonButton1.StateTracking.Border.Rounding = 15F;
-            kryptonButton1.StateTracking.Border.Width = 1;
-            kryptonButton1.TabIndex = 31;
-            kryptonButton1.Values.Text = "Next";
-            kryptonButton1.Visible = false;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.BackColor = Color.FromArgb(242, 242, 242);
-            label1.Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.ForeColor = SystemColors.ControlText;
-            label1.Location = new Point(108, 38);
-            label1.Name = "label1";
-            label1.Size = new Size(302, 19);
-            label1.TabIndex = 1;
-            label1.Text = "Which muscle groups would you like to train?";
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(0, 0);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(518, 262);
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
-            // 
-            // panel3
-            // 
-            panel3.Controls.Add(panel4);
-            panel3.Controls.Add(pictureBox2);
-            panel3.Controls.Add(pictureBox3);
-            panel3.Controls.Add(pictureBox4);
-            panel3.Controls.Add(label2);
-            panel3.Controls.Add(pictureBox5);
-            panel3.Location = new Point(86, 70);
-            panel3.Name = "panel3";
-            panel3.Size = new Size(518, 262);
-            panel3.TabIndex = 34;
-            // 
-            // panel4
-            // 
-            panel4.BackColor = Color.WhiteSmoke;
-            panel4.Controls.Add(kryptonButton2);
-            panel4.Location = new Point(214, 221);
-            panel4.Name = "panel4";
-            panel4.Size = new Size(90, 25);
-            panel4.TabIndex = 32;
-            // 
-            // kryptonButton2
-            // 
-            kryptonButton2.CornerRoundingRadius = 15F;
-            kryptonButton2.Location = new Point(0, 0);
-            kryptonButton2.Name = "kryptonButton2";
-            kryptonButton2.PaletteMode = Krypton.Toolkit.PaletteMode.ProfessionalSystem;
-            kryptonButton2.Size = new Size(90, 25);
-            kryptonButton2.StateCommon.Back.Color1 = Color.FromArgb(92, 225, 230);
-            kryptonButton2.StateCommon.Back.Color2 = Color.FromArgb(92, 225, 230);
-            kryptonButton2.StateCommon.Back.ColorAngle = 45F;
-            kryptonButton2.StateCommon.Border.Color1 = Color.FromArgb(92, 225, 230);
-            kryptonButton2.StateCommon.Border.Color2 = Color.FromArgb(92, 225, 230);
-            kryptonButton2.StateCommon.Border.ColorAngle = 45F;
-            kryptonButton2.StateCommon.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
-            kryptonButton2.StateCommon.Border.GraphicsHint = Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
-            kryptonButton2.StateCommon.Border.Rounding = 15F;
-            kryptonButton2.StateCommon.Border.Width = 1;
-            kryptonButton2.StateCommon.Content.ShortText.Color1 = Color.White;
-            kryptonButton2.StateCommon.Content.ShortText.Color2 = Color.White;
-            kryptonButton2.StateCommon.Content.ShortText.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Bold, GraphicsUnit.Point);
-            kryptonButton2.StateDisabled.Back.Color1 = Color.FromArgb(92, 225, 230);
-            kryptonButton2.StateDisabled.Back.Color2 = Color.FromArgb(92, 225, 230);
-            kryptonButton2.StateDisabled.Border.Color1 = Color.FromArgb(92, 225, 230);
-            kryptonButton2.StateDisabled.Border.Color2 = Color.FromArgb(92, 225, 230);
-            kryptonButton2.StateDisabled.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
-            kryptonButton2.StateNormal.Back.Color1 = Color.FromArgb(92, 225, 230);
-            kryptonButton2.StateNormal.Back.Color2 = Color.FromArgb(92, 225, 230);
-            kryptonButton2.StateNormal.Back.ColorAngle = 45F;
-            kryptonButton2.StateNormal.Back.GraphicsHint = Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
-            kryptonButton2.StateNormal.Border.Color1 = Color.FromArgb(92, 225, 230);
-            kryptonButton2.StateNormal.Border.Color2 = Color.FromArgb(92, 225, 230);
-            kryptonButton2.StateNormal.Border.ColorAngle = 45F;
-            kryptonButton2.StateNormal.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
-            kryptonButton2.StateNormal.Border.GraphicsHint = Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
-            kryptonButton2.StateNormal.Border.Rounding = 15F;
-            kryptonButton2.StateNormal.Border.Width = 1;
-            kryptonButton2.StatePressed.Back.Color1 = Color.FromArgb(0, 160, 192);
-            kryptonButton2.StatePressed.Back.Color2 = Color.FromArgb(0, 160, 192);
-            kryptonButton2.StatePressed.Back.ColorAngle = 45F;
-            kryptonButton2.StatePressed.Back.GraphicsHint = Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
-            kryptonButton2.StatePressed.Border.Color1 = Color.FromArgb(0, 160, 192);
-            kryptonButton2.StatePressed.Border.Color2 = Color.FromArgb(0, 160, 192);
-            kryptonButton2.StatePressed.Border.ColorAngle = 45F;
-            kryptonButton2.StatePressed.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
-            kryptonButton2.StatePressed.Border.GraphicsHint = Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
-            kryptonButton2.StatePressed.Border.Rounding = 15F;
-            kryptonButton2.StatePressed.Border.Width = 1;
-            kryptonButton2.StateTracking.Back.Color1 = Color.FromArgb(161, 234, 230);
-            kryptonButton2.StateTracking.Back.Color2 = Color.FromArgb(161, 234, 230);
-            kryptonButton2.StateTracking.Back.ColorAngle = 45F;
-            kryptonButton2.StateTracking.Back.GraphicsHint = Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
-            kryptonButton2.StateTracking.Border.Color1 = Color.FromArgb(161, 234, 230);
-            kryptonButton2.StateTracking.Border.Color2 = Color.FromArgb(161, 234, 230);
-            kryptonButton2.StateTracking.Border.ColorAngle = 45F;
-            kryptonButton2.StateTracking.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
-            kryptonButton2.StateTracking.Border.GraphicsHint = Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
-            kryptonButton2.StateTracking.Border.Rounding = 15F;
-            kryptonButton2.StateTracking.Border.Width = 1;
-            kryptonButton2.TabIndex = 31;
-            kryptonButton2.Values.Text = "Next";
-            kryptonButton2.Visible = false;
-            // 
-            // pictureBox2
-            // 
-            pictureBox2.Image = Properties.Resources.outdoors;
-            pictureBox2.Location = new Point(300, 161);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(83, 37);
-            pictureBox2.TabIndex = 4;
-            pictureBox2.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            pictureBox3.Image = Properties.Resources.home1;
-            pictureBox3.Location = new Point(218, 161);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(83, 37);
-            pictureBox3.TabIndex = 3;
-            pictureBox3.TabStop = false;
-            // 
-            // pictureBox4
-            // 
-            pictureBox4.Image = Properties.Resources.gym;
-            pictureBox4.Location = new Point(136, 161);
-            pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(83, 37);
-            pictureBox4.TabIndex = 2;
-            pictureBox4.TabStop = false;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.BackColor = Color.FromArgb(242, 242, 242);
-            label2.Font = new Font("Calibri", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.ForeColor = SystemColors.ControlText;
-            label2.Location = new Point(64, 31);
-            label2.Name = "label2";
-            label2.Size = new Size(428, 92);
-            label2.TabIndex = 1;
-            label2.Text = "Hello, ... . That is the workout section and we will \r\nask you a few questions in order to create the \r\nperfect workout for you.\r\nTo begin with, what place do you desire to workout at?";
-            label2.TextAlign = ContentAlignment.MiddleCenter;
-            // 
-            // pictureBox5
-            // 
-            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
-            pictureBox5.Location = new Point(0, 0);
-            pictureBox5.Name = "pictureBox5";
-            pictureBox5.Size = new Size(518, 262);
-            pictureBox5.TabIndex = 0;
-            pictureBox5.TabStop = false;
-            // 
             // Workouts
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(690, 368);
-            Controls.Add(panel1);
-            Controls.Add(panel3);
             Controls.Add(muscleGroupsPanel);
             Controls.Add(trainPlacePanel);
             Controls.Add(workoutsLabel);
@@ -694,17 +434,6 @@
             muscleGroupsPanel.PerformLayout();
             nextButtonPanel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)muscleGroupsBorders).EndInit();
-            panel1.ResumeLayout(false);
-            panel1.PerformLayout();
-            panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            panel3.ResumeLayout(false);
-            panel3.PerformLayout();
-            panel4.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -732,18 +461,5 @@
         private Label muscleGroupsLabel;
         private PictureBox muscleGroupsBorders;
         private ComboBox activityLevelComboBox;
-        private Panel panel1;
-        private Panel panel2;
-        private Krypton.Toolkit.KryptonButton kryptonButton1;
-        private Label label1;
-        private PictureBox pictureBox1;
-        private Panel panel3;
-        private Panel panel4;
-        private Krypton.Toolkit.KryptonButton kryptonButton2;
-        private PictureBox pictureBox2;
-        private PictureBox pictureBox3;
-        private PictureBox pictureBox4;
-        private Label label2;
-        private PictureBox pictureBox5;
     }
 }
